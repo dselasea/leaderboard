9123e9a8d3673fb5857e86f68b1456af434a3a9e
Daniel <dselasea@gmail.com>
2023-03-09 10:50:49 +0000

    Fix linter errors

--- src/modules/form.js
+++ src/modules/form.js
@@ -1,42 +1,42 @@
 import User from './userClass.js';
 
 // Dom Selectors
-  const form = document.querySelector('#form');
-  const userName = document.querySelector('#name');
-  const userScore = document.querySelector('#scores');
-  const refresh = document.querySelector('#refresh');
-  const scoresData = document.querySelector('.scores-list');
+const form = document.querySelector('#form');
+const userName = document.querySelector('#name');
+const userScore = document.querySelector('#scores');
+const refresh = document.querySelector('#refresh');
+const scoresData = document.querySelector('.scores-list');
 
-  // Clear userInput
-  const clearInput = () => {
-    userName.value = '';
-    userScore.value = '';
-  };
+// Clear userInput
+const clearInput = () => {
+  userName.value = '';
+  userScore.value = '';
+};
 
-  // Get userData
-  const getUserData = (e) => {
-    const user = new User(userName.value, userScore.value);
-    user.createUserScore(user);
-    clearInput();
-    e.preventDefault();
-  };
+// Get userData
+const getUserData = (e) => {
+  const user = new User(userName.value, userScore.value);
+  user.createUserScore(user);
+  clearInput();
+  e.preventDefault();
+};
 
-  // Get scores
-  const getScores = async () => {
-    let list = '';
-    const scores = await User.fetchScores();
-    const scoresList = scores.result;
-    scoresList.map((scorelist) => {
-      list += `<li><span>${scorelist.user}:</span> ${scorelist.score}</li>`;
-      return list;
-    });
-    scoresData.innerHTML = list;
-  };
+// Get scores
+const getScores = async () => {
+  let list = '';
+  const scores = await User.fetchScores();
+  const scoresList = scores.result;
+  scoresList.map((scorelist) => {
+    list += `<li><span>${scorelist.user}:</span> ${scorelist.score}</li>`;
+    return list;
+  });
+  scoresData.innerHTML = list;
+};
 
-  // Refresh to get scores from api
-  refresh.addEventListener('click', getScores);
+// Refresh to get scores from api
+refresh.addEventListener('click', getScores);
 
-  // Get form input
-  form.addEventListener('submit', getUserData);
+// Get form input
+form.addEventListener('submit', getUserData);
 
-  window.addEventListener('DOMContentLoaded', getScores);
+window.addEventListener('DOMContentLoaded', getScores);
