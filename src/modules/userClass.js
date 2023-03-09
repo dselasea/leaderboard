7269f88935cbed738cb153dee43903789ad744d4
Daniel <dselasea@gmail.com>
2023-03-09 10:02:02 +0000

    Fix linter errors, change id of score data in html

--- src/modules/userClass.js
+++ src/modules/userClass.js
@@ -5,18 +5,19 @@ export default class User {
     this.user = user;
     this.score = score;
   }
-  // Creates user scores 
+
+  // Creates user scores
   createUserScore = async (user) => {
     try {
-      const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/O6CvEZgnwfxjI9m8w7zH/scores`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    });
-    const data = await response.json();
-    return data;
+      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/O6CvEZgnwfxjI9m8w7zH/scores', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await response.json();
+      return data;
     } catch (error) {
       return error;
     }
@@ -24,40 +25,40 @@ export default class User {
 
   // Creates game with title
   static createGame = async () => {
-  try {
-    // Game object
-    const game = {
-      name: 'The Last Airbender'
-    }
-    const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body:JSON.stringify(game)
-  });
-  const data = await response.json();
-  return data;
-  } catch (error) {
-    return error;
-  }
-}
-
-  // Fetches scores
-  static fetchScores = async () => {
     try {
-      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/O6CvEZgnwfxjI9m8w7zH/scores', 
-      {
-        method: 'GET',
+    // Game object
+      const game = {
+        name: 'The Last Airbender',
+      };
+      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+        method: 'POST',
         headers: {
-          'Content-type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(game),
       });
-  
       const data = await response.json();
       return data;
     } catch (error) {
       return error;
     }
-  } 
+  }
+
+  // Fetches scores
+  static fetchScores = async () => {
+    try {
+      const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/O6CvEZgnwfxjI9m8w7zH/scores',
+        {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json',
+          },
+        });
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
 }
